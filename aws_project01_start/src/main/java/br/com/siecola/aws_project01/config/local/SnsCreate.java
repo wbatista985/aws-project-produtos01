fc3f50d3d80d08bfc8fa7bc4cefbdcfda0122c7b
Wagner Santos <wbatista985@gmail.com>
2024-06-20 14:59:03 -0300

    feat - criando SnsCreate

---
+++ aws_project01_start/src/main/java/br/com/siecola/aws_project01/config/local/SnsCreate.java
@@ -0,0 +1,39 @@
+package br.com.siecola.aws_project01.config.local;
+
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sns.AmazonSNS;
+import com.amazonaws.services.sns.AmazonSNSClient;
+import com.amazonaws.services.sns.model.CreateTopicRequest;
+import com.amazonaws.services.sns.model.Topic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("local")
+public class SnsCreate {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SnsCreate.class);
+
+    @Bean
+    public AmazonSNS snsClient() {
+        return AmazonSNSClient.builder()
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", Regions.US_EAST_1.getName()))
+                .withCredentials(new DefaultAWSCredentialsProviderChain())
+                .build();
+    }
+
+    @Bean(name = "productEventsTopic")
+    public Topic snsProductEventsTopic(AmazonSNS snsClient) {
+        CreateTopicRequest createTopicRequest = new CreateTopicRequest("product-events");
+        String productEventsTopicArn = snsClient.createTopic(createTopicRequest).getTopicArn();
+
+        LOG.info("Created SNS topic {}", productEventsTopicArn);
+        return new Topic().withTopicArn(productEventsTopicArn);
+    }
+
+}
