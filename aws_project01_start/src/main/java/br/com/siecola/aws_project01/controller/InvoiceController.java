b540a28c49cc9a121a8df2e82ba8a44f81885a9e
Wagner Santos <wbatista985@gmail.com>
2024-07-06 14:04:10 -0300

    feat - criando classe InvoiceController

---
+++ aws_project01_start/src/main/java/br/com/siecola/aws_project01/controller/InvoiceController.java
@@ -0,0 +1,62 @@
+package br.com.siecola.aws_project01.controller;
+
+import br.com.siecola.aws_project01.model.Invoice;
+import br.com.siecola.aws_project01.model.UrlResponse;
+import br.com.siecola.aws_project01.repository.InvoiceRepository;
+import com.amazonaws.HttpMethod;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/invoices")
+public class InvoiceController {
+
+    @Value("aws.s3.bucket.invoice.name")
+    private String bucketName;
+
+    private final AmazonS3 amazonS3;
+    private final InvoiceRepository invoiceRepository;
+
+    public InvoiceController(AmazonS3 amazonS3, InvoiceRepository invoiceRepository) {
+        this.amazonS3 = amazonS3;
+        this.invoiceRepository = invoiceRepository;
+    }
+
+    @PostMapping
+    public ResponseEntity<UrlResponse> createInvoiceUrl(){
+        UrlResponse urlResponse = new UrlResponse();
+        Instant expirationTime = Instant.now().plus(Duration.ofMinutes(5));
+        String processId = UUID.randomUUID().toString();
+
+        GeneratePresignedUrlRequest generatePresignedUrlRequest =
+                new GeneratePresignedUrlRequest(bucketName, processId)
+                        .withMethod(HttpMethod.PUT)
+                        .withExpiration(Date.from(expirationTime));
+
+        urlResponse.setExpirationTime(expirationTime.getEpochSecond());
+        urlResponse.setUrl(amazonS3.generatePresignedUrl(generatePresignedUrlRequest).toString());
+
+        return new ResponseEntity<>(urlResponse, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public Iterable<Invoice> getAllInvoices() {
+        return invoiceRepository.findAll();
+    }
+
+    @GetMapping(path = "/bycostumername")
+    public Iterable<Invoice> createInvoiceCostumerName(@RequestParam String costumerName){
+        return  invoiceRepository.findAllByCustomerName(costumerName);
+    }
+
+
+}
